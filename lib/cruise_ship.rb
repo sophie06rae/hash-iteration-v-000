627251455ac2848f2fc4e93746a544fa45df3dfa
Sophie Rae <sophie.rae.06@gmail.com>
2017-10-18 08:39:57 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,8 +1,8 @@
 # passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
+# suite_a: "Amanda Presley",
+# suite_b: "Seymour Hoffman",
+# suite_c: "Alfred Tennyson",
+# suite_d: "Charlie Chaplin",
 # suite_e: "Crumpet the Elf"
 # }
 
@@ -12,7 +12,7 @@ def select_winner(passengers)
 passengers.each do |suite, name|
   if suite == :suite_a && name.start_with? "a"
     winner = name
-  end 
+  end
 end
 
 winner
